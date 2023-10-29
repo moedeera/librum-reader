2649a112492bfa9aaa9a6d2e974ac9a138351e5a
moedeera <82620264+moedeera@users.noreply.github.com>
2023-10-29 08:07:40 -0600

    committ

--- src/Components/Block1/Block1.jsx
+++ src/Components/Block1/Block1.jsx
@@ -5,21 +5,21 @@ export const Block1 = () => {
   const info = [
     {
       id: 1,
-      cat: "Fantasy",
+      cat: "Fantasy 600",
       title: "Greenwood Castle",
       info: "Witch versus Vampires ",
       link: "/",
     },
     {
       id: 2,
-      cat: "Education",
+      cat: "Education 800",
       title: "Patents: A Short History",
       info: "A quick summary of Patents in the US",
       link: "/",
     },
     {
       id: 3,
-      cat: "Sci-fi",
+      cat: "Sci-fi 600",
       title: "Protocol Odyssey",
       info: "High space fantasy",
       link: "/",
