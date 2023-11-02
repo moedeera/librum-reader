15ec209d8adb243c75f5ca9c46ed37054769b5d5
moedeera <82620264+moedeera@users.noreply.github.com>
2023-11-02 00:00:31 -0600

    committ

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -20,9 +20,118 @@ export const SiteContextProvider = ({ children }) => {
     { id: 6, name: "Popular", link: "/" },
   ];
 
+  function parseHTMLContent(html) {
+    const container = document.createElement("div");
+    container.innerHTML = html;
+
+    const result = [];
+
+    function traverse(node) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        const text = node.textContent.trim();
+        if (text) {
+          result.push({ tag: "p", type: "regular", content: text });
+        }
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        const tag = node.tagName.toLowerCase();
+        const type =
+          node.tagName === "B"
+            ? "bold"
+            : node.tagName === "I"
+            ? "italic"
+            : "regular";
+        const content = node.textContent.trim();
+
+        result.push({ tag, type, content });
+      }
+
+      if (node.childNodes) {
+        for (const child of node.childNodes) {
+          traverse(child);
+        }
+      }
+    }
+
+    for (const element of container.childNodes) {
+      traverse(element);
+    }
+
+    return result;
+  }
+
+  function parseHtmlToQuillDelta(html) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    const quillDelta = [];
+
+    const parseNode = (node) => {
+      const insert = {};
+
+      if (node.nodeType === Node.TEXT_NODE) {
+        const text = node.textContent.replace(/\n/g, "\n"); // Replace newline characters
+        if (text.trim()) {
+          insert.insert = text;
+          quillDelta.push(insert);
+        }
+      } else if (node.nodeType === Node.ELEMENT_NODE) {
+        const tagName = node.tagName;
+
+        switch (tagName) {
+          case "B":
+          case "STRONG":
+            insert.insert = node.textContent;
+            insert.attributes = { bold: true };
+            quillDelta.push(insert);
+            break;
+          case "I":
+          case "EM":
+            insert.insert = node.textContent;
+            insert.attributes = { italic: true };
+            quillDelta.push(insert);
+            break;
+          case "U":
+            insert.insert = node.textContent;
+            insert.attributes = { underline: true };
+            quillDelta.push(insert);
+            break;
+          case "A":
+            insert.insert = node.textContent;
+            insert.attributes = { link: node.getAttribute("href") };
+            quillDelta.push(insert);
+            break;
+          case "BR":
+            insert.insert = "\n";
+            quillDelta.push(insert);
+            break;
+          default:
+            if (node.childNodes.length > 0) {
+              const childNodes = node.childNodes;
+              childNodes.forEach((childNode) => parseNode(childNode));
+            }
+            break;
+        }
+      }
+    };
+
+    doc.body.childNodes.forEach((node) => parseNode(node));
+
+    return quillDelta;
+  }
+
+  // const htmlContent = "<b>Hello </b><strong>World!</strong><br>";
+  // const quillDelta = parseHtmlToQuillDelta(htmlContent);
+  // console.log(JSON.stringify(quillDelta, null, 2));
+
   return (
     <SiteContext.Provider
-      value={{ menuItemsMD, menuItemsLG, imagesSorted, findImageSet }}
+      value={{
+        menuItemsMD,
+        menuItemsLG,
+        imagesSorted,
+        findImageSet,
+
+        parseHtmlToQuillDelta,
+      }}
     >
       {children}
     </SiteContext.Provider>
--- src/Pages/PostStory/PostStory.jsx
+++ src/Pages/PostStory/PostStory.jsx
@@ -1,20 +1,58 @@
 import "quill/dist/quill.snow.css";
 import Quill from "quill";
-import { useCallback } from "react";
+import { useCallback, useContext, useState } from "react";
 import "./PostStory.css";
+import { SiteContext } from "../../Context/Context";
 
 export const PostStory = () => {
+  const [postData, setPostData] = useState("");
+  const { parseHtmlToQuillDelta } = useContext(SiteContext);
+
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper === null) return;
-    wrapper.innerHTML = "";
+
+    // Create a new Quill editor
     const editor = document.createElement("div");
+    editor.setAttribute("id", "quill-editor");
     wrapper.append(editor);
-    new Quill(editor, {
+
+    // Initialize Quill on the editor
+    const quill = new Quill(editor, {
       theme: "snow",
     });
+    const savedData = localStorage.getItem("savedData");
+    if (savedData && savedData !== null) {
+      console.log(JSON.parse(savedData));
+      quill.setContents(JSON.parse(savedData));
+    } else {
+      quill.setText("Hello\n");
+    }
 
+    const button = document.getElementById("save-button");
+
+    button.addEventListener("click", () => {
+      //   const content = quill.root.innerHTML;
+      //   const data = parseHtmlToQuillDelta(content);
+
+      //   console.log(data);
+      var delte = quill.getContents();
+      console.log("content:", ":", delte.ops);
+      localStorage.setItem("savedData", JSON.stringify(delte.ops));
+    });
+
+    // Log new characters when the user edits the content
+    quill.on("text-change", (delta, oldDelta, source) => {
+      if (source === "user") {
+        // This logs the changes made by the user
+        const content = quill.root.innerHTML;
+        setPostData(content);
+      }
+    });
+
+    // Cleanup function
     return () => {
-      wrapperRef.innerHTML = "";
+      quill.off("text-change");
+      wrapper.innerHTML = "";
     };
   }, []);
 
@@ -22,10 +60,18 @@ export const PostStory = () => {
     <div className="container">
       <div className="text-editor-page">
         <h3>Your Story</h3>
-        <div className="text-editor" ref={wrapperRef}></div>
+        <div
+          className="text-editor"
+          ref={wrapperRef}
+          id="quill-container"
+        ></div>
         <div className="editor-button-container">
-          <button className="btn btn-primary">Preview</button>
-          <button className="btn btn-primary btn-green">Save</button>
+          <button className="btn btn-primary" onChangeCapture={() => {}}>
+            Preview
+          </button>
+          <button id="save-button" className="btn btn-primary btn-green">
+            Save
+          </button>
         </div>
       </div>
     </div>
