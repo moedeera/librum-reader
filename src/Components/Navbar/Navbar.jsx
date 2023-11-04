230c9cd1e6d9d2d14be84baa29efa29714be9d7b
Moe Deera <deeramoe@gmail.com>
2023-11-03 22:22:07 -0600

    fixing colors issue
    color

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { SiteContext } from "../../Context/Context";
 
 export const Navbar = () => {
-  const { menuItemsMD, menuItemsLG } = useContext(SiteContext);
+  const { menuItemsMD } = useContext(SiteContext);
 
   return (
     <div className="navbar-container">
