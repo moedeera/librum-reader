17de76b5665f4d4d43a2097b0bb6406d5e855b9c
Moe Deera <deeramoe@gmail.com>
2023-11-11 02:10:45 -0600

    Profile page
    delete section done

--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,28 +1,17 @@
 import "./ProfilePage.css";
-import { Link } from "react-router-dom";
+
 import { BasicInfo } from "./ProfilePageComp/BasicInfo";
+import { DeleteProfile } from "./ProfilePageComp/DeleteProfile";
+import { MessagesBox } from "./ProfilePageComp/MessagesBox";
 import { PasswordReset } from "./ProfilePageComp/PasswordReset";
 export const ProfilePage = () => {
   return (
     <div className="container">
       <div className="profile-page-container">
-        <div className="profile-page-segment">
-          <h4>Messages</h4>
-          <div className="latest">
-            <p></p>
-            <button className="btn btn-primary">Read Latest</button>
-          </div>
-          <div className="all">
-            <Link to={"/"} className="btn btn-primary">
-              See All
-            </Link>
-          </div>
-        </div>
+        <MessagesBox />
         <PasswordReset />
-
         <BasicInfo />
-
-        <div className="profile-page-segment"></div>
+        <DeleteProfile />
         <div className="profile-page-view"></div>
       </div>
     </div>
