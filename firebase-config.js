b7943a6f6d6fa2b2e2a85d0c2f5248fa191cc9fb
Moe Deera <deeramoe@gmail.com>
2023-11-11 03:16:58 -0600

    committ

---
+++ firebase-config.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+import { getStorage, ref } from "firebase/storage";
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCH49nJNK0-SinR4VT6V0a-_FZsapLgtJ8",
+  authDomain: "portfolio-33f0f.firebaseapp.com",
+  projectId: "portfolio-33f0f",
+  storageBucket: "portfolio-33f0f.appspot.com",
+  messagingSenderId: "170316975789",
+  appId: "1:170316975789:web:f8c5aaff8e7199f7920024",
+  measurementId: "G-M0ZQL4395P",
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const signInWithGoogle = () => {
+  signInWithPopup(auth, provider);
+};
+
+export { app, db };
