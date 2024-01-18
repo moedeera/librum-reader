5f18796f91abc80e789711f3379b8d140bd4190a
Moe Deera <deeramoe@gmail.com>
2024-01-17 22:20:01 -0600

    committ

--- src/Components/Block6/Block6.jsx
+++ src/Components/Block6/Block6.jsx
@@ -1,14 +1,34 @@
 import "./Block6.css";
+import img1 from "./invite.svg";
+import img2 from "./buffer.svg";
+import img3 from "./developer.svg";
+import img4 from "./math.svg";
+import { Link } from "react-router-dom";
 
 export const Block6 = () => {
-  const promo = ["Science", "Biographies", "Essays", "Short-Stories"];
+  const cards = [
+    { name: "Improve Writing", image: img3 },
+    { name: "Collaborate", image: img1 },
+    { name: "Share Online", image: img2 },
+    { name: "Non-Fiction", image: img4 },
+  ];
+
   return (
     <div className="block-6-container">
+      <small>Learn more</small>
+      <h3>Get Inspired</h3>
       <div className="block-6-segments-container">
-        {promo.map((item, index) => (
+        {cards.map((card, index) => (
           <div key={index} className="block-6-segment">
-            <img src="" alt="" />
-            <h3>{item}</h3>
+            <img src={card.image} alt="" />
+            <h4>{card.name}</h4>
+            <p>
+              A unique platform for writers of all levels to enhance their
+              craft.
+            </p>
+            <Link className="btn" to={"/"}>
+              Learn More
+            </Link>
           </div>
         ))}
       </div>
