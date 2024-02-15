c53b9e881a9418242ab4c54ad58f6bfe564b4ab5
Moe Deera <deeramoe@gmail.com>
2024-02-15 02:48:45 -0600

    committ

--- src/Components/Block4/Block4.jsx
+++ src/Components/Block4/Block4.jsx
@@ -8,6 +8,8 @@ import { db } from "../../../firebase-config";
 
 // eslint-disable-next-line react/prop-types
 export const Block4 = ({ summaries, showingAmount }) => {
+  const dummyImageUrl =
+    "https://images.pexels.com/photos/20197333/pexels-photo-20197333/free-photo-of-a-man-in-cowboy-hat-riding-a-horse-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
   const updateViews = async (id) => {
     console.log(id);
     const story = doc(db, "stories", id);
@@ -90,10 +92,21 @@ export const Block4 = ({ summaries, showingAmount }) => {
                 }
               ></div>
               <div className="block-4-segment-text">
+                <div
+                  className="block-4-segment-image"
+                  style={{
+                    backgroundImage: `url("${item.pic}")`,
+                  }}
+                >
+                  {/* <img
+                    src="https://images.pexels.com/photos/20197333/pexels-photo-20197333/free-photo-of-a-man-in-cowboy-hat-riding-a-horse-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    alt=""
+                  /> */}
+                </div>
                 <small>{item.cat}</small>
-                <h4>{item.title}</h4>
+                <h5>{item.title}</h5>
                 <p>{item.info}</p>
-                <div className="btn">Read More</div>
+                {/* <div className="btn">Read More</div> */}
                 <p className="block-4-tags">
                   Tags:
                   {item?.tag?.map((tagItem, index) => (
