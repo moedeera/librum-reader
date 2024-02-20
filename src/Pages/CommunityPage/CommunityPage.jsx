3227f11f33a0922695ebdffed13086d0944b1516
Moe Deera <deeramoe@gmail.com>
2024-02-20 05:47:41 -0600

    committ

--- src/Pages/CommunityPage/CommunityPage.jsx
+++ src/Pages/CommunityPage/CommunityPage.jsx
@@ -1,11 +1,14 @@
+import { Block1 } from "../../Components/Block1/Block1";
 import { Block3 } from "../../Components/Block3/Block3";
 import { b3content } from "../../Context/Content";
 import "./CommunityPage.css";
 
 export const CommunityPage = () => {
   return (
-    <div className="container standard-page">
+    <div className="container community-page standard-page">
+      <h3>Join the Community</h3>
       <Block3 data={b3content} />
+      <Block1 />
     </div>
   );
 };
