000924e144588113185015dbf93c5216b5489de2
moedeera <82620264+moedeera@users.noreply.github.com>
2024-04-13 07:06:53 -0600

    COMMITT

--- src/Pages/AuthPage.js/AuthPage.jsx
+++ src/Pages/AuthPage.js/AuthPage.jsx
@@ -1,5 +1,12 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+
 const AuthPage = () => {
-  return <div>AuthPage</div>;
+  const [post, setPost] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  return <div className="container">AuthPage</div>;
 };
 
 export default AuthPage;
