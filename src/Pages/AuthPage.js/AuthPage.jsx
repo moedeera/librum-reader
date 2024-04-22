6332761a857fcb3d1e687169bda4fdac45e665f1
Moe Deera <deeramoe@gmail.com>
2024-04-21 20:53:47 -0600

    committ

--- src/Pages/AuthPage.js/AuthPage.jsx
+++ src/Pages/AuthPage.js/AuthPage.jsx
@@ -275,7 +275,62 @@ const AuthPage = () => {
       setLoading(false);
     }
   };
+  // create draft
+  const handleCreateDraft = async () => {
+    let account;
+    console.log(user);
+    const accountCollection = collection(db, "accounts");
+    try {
+      const q = query(accountCollection, where("userId", "==", user.uid));
+      const querySnapshot = await getDocs(q);
+      setLoading(true);
+      if (querySnapshot.empty) {
+        throw new Error("No such profile");
+      } else {
+        account = querySnapshot.docs[0];
+        console.log(account.data());
+      }
 
+      const profile = await handleFetchProfile();
+      console.log(profile);
+      let initialSLug = `alovelyday`;
+
+      const finalSlug = appendStringWithDateTime(initialSLug, profile.url);
+      let newDraft = {
+        authorName: user.displayName,
+        userId: user.uid,
+        authorPic: user.photoURL,
+        category: "Fiction",
+        authorLink: profile.url,
+        dateCreated: new Date(),
+        title: "A lovely town",
+        synopsis:
+          "A lovely town by the Alpine river encounters a mysterious wizard warning them of impending war",
+        cover:
+          "https://firebasestorage.googleapis.com/v0/b/librum-reader.appspot.com/o/images%2Flittle-girl-running-795505_1280.jpg?alt=media&token=c5ba4877-7d6d-40e0-a7c2-c2108c09e5db",
+        slug: finalSlug,
+        story: loremIpsum,
+        promoted: false,
+        wordCount: "",
+        stats: [0, 0, 0],
+      };
+
+      const createdDraft = await addDoc(draftsCollection, newDraft);
+      let updatedDrafts = [
+        ...account.data().drafts,
+        {
+          slug: finalSlug,
+          title: "A lovely town",
+          cover:
+            "https://firebasestorage.googleapis.com/v0/b/librum-reader.appspot.com/o/images%2Flittle-girl-running-795505_1280.jpg?alt=media&token=c5ba4877-7d6d-40e0-a7c2-c2108c09e5db",
+        },
+      ];
+      await updateDoc(account.ref, { drafts: updatedDrafts });
+      console.log(createdDraft);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
