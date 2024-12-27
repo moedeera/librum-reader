c37fb0eef06b601ba232cda45d32246ddcffa31a
moedeera <82620264+moedeera@users.noreply.github.com>
2024-12-27 02:08:47 -0600

    contact component
    contact component set up

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,16 +1,69 @@
 import "./Contact.css";
+import { useState } from "react";
+import { useMessages } from "@/utils/custom-hooks/useMessages";
+import { Loading } from "../Loading/Loading";
 
 export const Contact = () => {
+  const { createMessage } = useMessages();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    if (!name || !email || !message) {
+      alert("All fields are required.");
+      return;
+    }
+    setLoading(true);
+    try {
+      await createMessage(name, email, message);
+      setLoading(false);
+      alert("Message sent successfully.");
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (error) {
+      console.error("Error sending message:", error);
+      alert("Failed to send message. Please try again later.");
+    }
+  };
+
   return (
-    <div className="contact-block">
-      <h3>Have any Questions? Want to Collaborate? Send Us a Message</h3>
-      <p>Name</p>
-      <input type="text" placeholder="Name" />
-      <p>Email</p>
-      <input type="text" placeholder="Email" />
-      <p>Message</p>
-      <textarea rows={10} />
-      <button className="btn btn-primary">Send</button>
-    </div>
+    <>
+      {loading ? (
+        <>
+          <Loading />
+        </>
+      ) : (
+        <div className="contact-block">
+          <h3>Have any Questions? Want to Collaborate? Send Us a Message</h3>
+          <p>Name</p>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <p>Email</p>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <p>Message</p>
+          <textarea
+            rows={10}
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button className="btn btn-primary" onClick={handleSubmit}>
+            Send
+          </button>
+        </div>
+      )}
+    </>
   );
 };
---
+++ src/utils/custom-hooks/useMessages.jsx
@@ -0,0 +1,69 @@
+import { useContext, useState } from "react";
+import { db } from "../../../firebase-config";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  addDoc,
+} from "firebase/firestore";
+import { AuthContext } from "@/Context/AuthContext";
+
+export const useMessages = () => {
+  const { currentUser } = useContext(AuthContext);
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Function to create a new message
+  const createMessage = async (name, email, message) => {
+    try {
+      const messagesRef = collection(db, "messages");
+      const newMessage = {
+        name,
+        email,
+        message,
+
+        timestamp: Date.now(),
+      };
+      await addDoc(messagesRef, newMessage);
+      console.log("Message sent successfully.");
+    } catch (error) {
+      console.error("Error creating message:", error);
+      throw error;
+    }
+  };
+
+  // Function to fetch messages for admin user only
+  const fetchMessagesForAdmin = async () => {
+    setLoading(true);
+    try {
+      const adminUserId = "vHbYS7VrHDMR4JldgnHnXzp1KYT2";
+      const messagesRef = collection(db, "messages");
+      const q = query(
+        messagesRef,
+        where("userId", "==", adminUserId),
+        orderBy("timestamp", "desc")
+      );
+      const querySnapshot = await getDocs(q);
+      const fetchedMessages = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMessages(fetchedMessages);
+      return fetchedMessages;
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    messages,
+    loading,
+    createMessage,
+    fetchMessagesForAdmin,
+  };
+};
